7528f14c82ab168c85c7d404f6139bd802e8585a
MaksZ <max.zabierowski@gmail.com>
2026-05-21 14:03:45 +0200

    Added loading json configuration, or default values

---
+++ RadarProcessor.m
@@ -0,0 +1,36 @@
+classdef RadarProcessor < handle
+
+    properties (Access = public)
+        Fs (1,1) double = 10e6;     % 10MHz for DVB-T channel
+        Fc (1,1) double = 500e6;    % popular DVB-t station
+        x_ref (:,1) double
+        x_surv (:,1) double
+
+        CAF_matrix = double
+    end
+
+    methods 
+        function obj = RadarProcessor(initialFs, initialFc)
+            obj.Fs = initialFs;
+            obj.Fc = initialFc;
+        end
+        
+        function obj = loadRef(ref)
+            obj.x_ref = ref;
+        end
+        
+        function obj = loadSurv(surv)
+            obj.x_surv = surv;
+        end
+
+        function obj = loadSignals(ref,surv)
+            obj.x_ref = ref;
+            obj.x_surv = surv;
+        end
+
+        function obj = calculateCAF()
+
+            
+        end
+    end
+end
