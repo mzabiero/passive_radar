47a2c961fa1fef913ba6bc10d9665cc884758b8d
Maks Z <1178463@pw.edu.pl>
2026-06-27 17:45:54 +0200

    Working reading all files, and plotting xcorr, TODO: Creating processing in Radar Engine (plotting ref, surv: time and freq

--- +Parsers/Is3pFileParser.m
+++ +Parsers/Is3pFileParser.m
@@ -1,10 +1,14 @@
 classdef Is3pFileParser < Parsers.BaseFileParser
     methods
-        function [ref, surv, metadata, success] = parseFile(obj, filePath)
+        function [ref, surv, metadata, success] = parseFile(~, filePath)
             try
                 filePath = string(filePath);
                 [data, header] = Utils.parseRaw(filePath);
-                ref = 
+                ref = data(:,2,:);
+                surv = data(:,1,:);
+                ref = ref(:);
+                surv = surv(:);
+                metadata = header;
                 success = true;
             catch ME
                 ref = []; surv = []; metadata = []; success = false;
