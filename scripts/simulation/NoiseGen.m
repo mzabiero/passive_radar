cf4ec8a1f6756f2efc8ecd7aa927c8aabba96ec2
Maks Zabierowski xy <max.zabierowski@gmail.com>
2026-08-20 22:41:57 +0200

    feat: VelCut indexs edit field

--- scripts/simulation/NoiseGen.m
+++ scripts/simulation/NoiseGen.m
@@ -1,7 +1,7 @@
 clear all; close all; clc
 
 T = 0.5;
-fs = 10e6;
+fs = 20e6;
 fc = 500e6;
 N = fs * T;
 f = 10;
@@ -19,9 +19,10 @@ x_ref = randn(N,1) + 1i* randn(N,1);
 x_surv = zeros(N,1);
 
 idx = tgt_del_samp:N;
-x_surv(idx) = x_ref(1:end-tgt_del_samp+1); %.* exp(1j * 2 * pi * tgt_fd * t(idx));
-x_surv_comp = x_surv .* exp(1j * 2 * pi * tgt_fd * t);
+%x_surv(idx) = x_ref(1:end-tgt_del_samp+1); %.* exp(1j * 2 * pi * tgt_fd * t(idx));
+%x_surv_comp = x_surv .* exp(1j * 2 * pi * tgt_fd * t);
+Gui.plotSpectrum(x_ref, fs, fc);
 %x_surv = x_surv * exp(1j * 2 * pi / lambda * tgt_v * t);
-[x_cor, lags] = xcorr(x_surv, x_ref, 200);
-plot(lags, abs(x_cor));
+%[x_cor, lags] = xcorr(x_surv, x_ref, 200);
+%plot(lags, abs(x_cor));
 %plot(t,abs(x));
